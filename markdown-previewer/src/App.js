b43777d4a9974434877629f1be0af2892b08f13b
Rifat Hossain <rifat.hossain.dhrubo@gmail.com>
2020-04-26 13:45:10 +0600

    added editor and preview maximize,minimize options

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -1,19 +1,52 @@
 import React, { useState } from 'react';
 import styled from '@emotion/styled';
 import ReactMarkdown from 'react-markdown/with-html';
+import { TiArrowMaximise } from 'react-icons/ti';
 
 function App() {
   const [markDown, setMarkDown] = useState('');
+  const [editorDisplay, setEditorDisplay] = useState('flex');
+  const [editorSize, setEditorSize] = useState('30vh');
+  const [previewDisplay, setPreviewDisplay] = useState('flex');
+  const [previewSize, setPreviewSize] = useState('50vh');
 
-  const handleEditorChange = (event) => {
+  const handleInput = (event) => {
     setMarkDown(event.target.value);
   };
+
+  const handleEditor = () => {
+    if (editorDisplay === 'flex' && editorSize === '30vh') {
+      setPreviewDisplay('none');
+      setEditorSize('90vh');
+    } else {
+      setPreviewDisplay('flex');
+      setEditorSize('30vh');
+    }
+  };
+  const handlePreview = () => {
+    if (previewDisplay === 'flex' && previewSize === '50vh') {
+      setEditorDisplay('none');
+      setPreviewSize('90vh');
+    } else {
+      setEditorDisplay('flex');
+      setPreviewSize('50vh');
+    }
+  };
+
   return (
     <Wrapper>
-      <Editor>
-        <textarea id="textArea" onChange={handleEditorChange} />
+      <Editor display={editorDisplay} minHeight={editorSize}>
+        <span>
+          <TiArrowMaximise onClick={handleEditor} />
+        </span>
+        <textarea id="textArea" onChange={handleInput} />
       </Editor>
-      <Preview source={markDown} escapeHtml={false} />
+      <Preview display={previewDisplay} minHeight={previewSize}>
+        <span>
+          <TiArrowMaximise onClick={handlePreview} />
+        </span>
+        <ReactMarkdown source={markDown} escapeHtml />
+      </Preview>
     </Wrapper>
   );
 }
@@ -30,9 +63,20 @@ const Wrapper = styled.div({
 const Editor = styled.div(
   {
     background: '#f5f5f5',
-    minHeight: '30vh',
+
     fontFamily: 'inherit',
     padding: '1rem',
+    margin: '1rem 0',
+    flexDirection: 'column',
+
+    span: {
+      display: 'flex',
+      justifyContent: 'flex-end',
+
+      svg: {
+        cursor: 'pointer',
+      },
+    },
 
     textArea: {
       background: '#f5f5f5',
@@ -47,15 +91,33 @@ const Editor = styled.div(
     },
   },
   (props) => ({
-    display: props.display || 'block',
+    display: props.display || 'flex',
+    minHeight: props.minHeight || '30vh',
   })
 );
 
-const Preview = styled(ReactMarkdown)({
-  background: '#f5f5f5',
-  minHeight: '60vh',
-  overflowY: 'scroll',
-  padding: '1rem',
-});
+const Preview = styled.div(
+  {
+    background: '#f5f5f5',
+
+    overflowY: 'scroll',
+    padding: '1rem',
+    margin: '1rem 0',
+    flexDirection: 'column',
+
+    span: {
+      display: 'flex',
+      justifyContent: 'flex-end',
+
+      svg: {
+        cursor: 'pointer',
+      },
+    },
+  },
+  (props) => ({
+    display: props.display || 'flex',
+    minHeight: props.minHeight || '50vh',
+  })
+);
 
 export default App;
